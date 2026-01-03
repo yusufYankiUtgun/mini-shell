2abe1cea68a67e36c623e481faee64b00d9277b5
Yusuf Yanki Utgun <utguny@gmail.com>
2026-01-03 20:08:20 +0300

    feat: implement dynamic buffer allocation for input handling
    - switch from fgets() to read() for better control over input
    - add automatic buffer resizing when capacity exceeded
    - handle EOF during multi-chunk line reading
    - add explicit fflush() after prompt
    - skip empty commands and improve error handling

--- mini-shell/kell.c
+++ mini-shell/kell.c
@@ -3,7 +3,7 @@
 
 
 
-void* resize(void* ptr, int new_size) {
+void* resize(void* ptr, size_t new_size) {
 
     void* temp_ptr = realloc(ptr, new_size);
 
@@ -11,33 +11,80 @@ void* resize(void* ptr, int new_size) {
         perror("kell: realloc");
         return NULL;
     }
-
+    
     return temp_ptr;
 
 }
 
 
 
-
-
-
-
 int main(int argc, char** argv) {
 
-    char buff[1024];
+    size_t buff_capacity = 2 * sizeof(char);
+    char* buff = malloc(buff_capacity);
+    
 
     while (1) {
         
+        int flag = 1;
+
         printf("kell$ ");
-        fgets(buff, BUFF_MAX, stdin);
+        fflush(stdout);
+
+
+        int bytes_read = read(0, buff, buff_capacity - 1);
+        
+        if (bytes_read == 0) {
+            printf("\n");
+            exit(EXIT_SUCCESS);
+        }
+        
+        buff[bytes_read] = '\0';
+        
+        while(buff[strlen(buff) - 1] != '\n') {
+            
+            int current_length = strlen(buff);
+
+            if (strlen(buff) == buff_capacity - 1) {
+                
+                char* tmp = resize(buff, buff_capacity * 2);
+                
+                
+                if (tmp != NULL) {
+                    buff = tmp;
+                    buff_capacity *= 2;                                                         
+                } else {
+                    printf("not enough memory");
+                    continue;
+                }
+
+            }
+            
+
+            bytes_read = read(0 , buff + current_length, buff_capacity - current_length - 1);
+            
+            if (bytes_read == 0) {
+                flag = 0;
+                break;
+            }
+        
+            buff[current_length + bytes_read] = '\0';
+            
+
+        }
+        
+
         char* argsToSendExec[MAX_ARGC];
         
         int a = 0;
-        buff[strlen(buff) - 1] = '\0';
+        
+        if (flag) {
+            buff[strlen(buff) - 1] = '\0';
+        }
+        
         char* token = strtok(buff, " ");
 
         
-
         while (token != NULL) {
 
             argsToSendExec[a++] = token;
@@ -47,38 +94,32 @@ int main(int argc, char** argv) {
 
         argsToSendExec[a] = NULL;
 
+        if (a == 0) {
+            continue;
+        }
+
         pid_t cpid = fork();
         
 
-
-
         if (cpid < 0) {
             perror("fork");
-            return EXIT_FAILURE;
+            exit(EXIT_FAILURE);
         }
         
         int status;
 
         if (cpid == 0) {
-
+            if (!flag) {
+                printf("\n");
+            } 
             if (execvp(argsToSendExec[0], argsToSendExec) == -1) {
                 perror("execvp");
-                return EXIT_FAILURE;
+                exit(EXIT_FAILURE);
             }
 
         } 
 
         wait(&status);
     }
-        
-
-
-
-        
-    
-
-
-
-
     return EXIT_SUCCESS;
 }
